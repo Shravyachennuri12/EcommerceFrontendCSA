55554d0a166ed741e4fe5eae1d49a06cdff3a959
shravya <chennurishravya088@gmail.com>
2024-08-16 19:00:48 +0530

    Css file updated

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './ProductPage.css';
+import './Productpage.css';
 
 const ProductPage = () => {
   const [products, setProducts] = useState([]);
